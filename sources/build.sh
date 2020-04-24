86475e7dbe17ea215e4578535851a5b5009185ac
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-04-24 15:34:28 +0100

    Update build.sh

--- sources/build.sh
+++ sources/build.sh
@@ -7,13 +7,15 @@ DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 cd $DIR
 echo $(pwd)
 
+rm -rf ../fonts
+
 echo "Generating Static fonts"
 mkdir -p ../fonts
 fontmake -m Heebo.designspace -i -o ttf --output-dir ../fonts/ttf/
 fontmake -m Heebo.designspace -i -o otf --output-dir ../fonts/otf/
 
 echo "Generating VFs"
-mkdir -p ../fonts/vf
+mkdir -p ../fonts/variable
 fontmake -m Heebo.designspace -o variable --output-path ../fonts/variable/Heebo[wght].ttf
 
 
@@ -38,32 +40,35 @@ do
 	# ./ttfautohint-vf -l 8 -r 50 -G 200 -x 14 -D hebr -f latn -W -c --stem-width-mode nnn $vf "$vf.fix";
 	# mv "$vf.fix" $vf;
 done
+echo $(ls ../fonts/variable)
 
 
 
 echo "Fixing VF Meta"
 gftools fix-vf-meta $vfs;
+for vf in $vfs
+do
+	mv $vf.fix $vf;
+done
+
+echo $(ls ../fonts/variable)
 
 echo "Dropping MVAR"
 for vf in $vfs
 do
-	mv "$vf.fix" $vf;
-	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
-	rtrip=$(basename -s .ttf $vf)
-	new_file=../fonts/variable/$rtrip.ttx;
-	rm $vf;
-	ttx $new_file
-	rm $new_file
+	gftools fix-unwanted-tables -t MVAR $vf;
 done
 
 echo "Fixing Hinting"
 for vf in $vfs
 do
 	gftools fix-nonhinting $vf $vf;
-	# mv "$vf.fix" $vf;
 done
+echo $(ls ../fonts/variable)
 for ttf in $ttfs
 do
 	gftools fix-nonhinting $ttf $ttf;
 	# mv "$ttf.fix" $ttf;
 done
+
+rm -f ../fonts/ttf/*gasp.ttf ../fonts/variable/*gasp.ttf
